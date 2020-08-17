9e1e42ac394ea916edd9874cae15cf6451e9a843
calebfonyuy <fonyuyca@gmail.com>
2020-08-17 08:55:34 +0100

    File Logging

--- astm/logging.py
+++ astm/logging.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from pathlib import Path
+from shutil import move
 
 """
 To control logging with command line commands, consider using 
@@ -8,12 +10,32 @@ been provided.
 
 
 def server_log(info):
-    print(datetime.now(), "Server:", info)
+    log = str(datetime.now())+" Server: "+str(info)
+    __write_log_file(log)
+    print(log)
 
 
 def client_log(info):
-    print(datetime.now(), "Client", info)
+    log = str(datetime.now())+" Client: "+str(info)
+    __write_log_file(log)
+    print(log)
 
 
 def common_log(info):
-    print(datetime.now(), info)
+    log = str(datetime.now())+": "+str(info)
+    __write_log_file(log)
+    print(log)
+
+
+def __write_log_file(log):
+    file = open("astm_log.log",'a')
+    file.write(log+"\n")
+    file.close()
+    path = Path('astm_log.log').stat()
+    if path.st_size >=3000000:
+        move('astm_log.log', "astm_log_"+str(datetime.now())+".log")
+    
+
+if __name__=="__main__":
+    log = str(datetime.now())+" Client: Test Write"
+    __write_log_file(log)
