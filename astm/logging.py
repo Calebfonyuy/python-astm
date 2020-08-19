ffd4bf929c8b781b21a6b40f0c7e87f1ba706a1b
calebfonyuy <fonyuyca@gmail.com>
2020-08-19 08:54:31 +0100

    Added deletion of old log files

--- astm/logging.py
+++ astm/logging.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from pathlib import Path
 from shutil import move
-from os import listdir
+from os import listdir, remove
 import re
 
 """
@@ -34,14 +34,24 @@ def __write_log_file(log):
     file.write(log+"\n")
     file.close()
     path = Path('astm_log.log').stat()
-    if path.st_size >=3000000:
+    if path.st_size >=5000000:
         move('astm_log.log', "astm_log_"+str(datetime.now())+".log")
     today = datetime.now().day
-    if day==1:
+    if today==1:
         month = datetime.now().month
-        year = datetime.now().year
-        all_logs = [f for f in os.listdir(".") if len(re.findall('[0-9- :.]+[0-9]+',str(f)))>0]
+        #ensure that records for november and december can be effectively deleted
+        if month < 3:
+            month += 12
+        all_logs = [f for f in listdir(".") if len(re.findall('[0-9- :.]+[0-9]+',str(f)))>0]
+        del_list = []
+        for item in all_logs:
+            yr = int(re.findall('_([0-9]+)-',item)[0])
+            mon = int(re.findall('-([0-9]+)-',item)[0])
+            if mon < month-1:
+                del_list.append(item)
         
+        for item in del_list:
+            remove(item)
     
 
 if __name__=="__main__":
